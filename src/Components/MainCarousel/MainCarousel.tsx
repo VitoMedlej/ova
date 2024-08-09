e0ac90fc284993e7b2becb5479495dffc4783ec9
Vito Medlej <vito.medlej@gmail.com>
2024-08-09 13:08:26 +0300

    imaegs dynamic

--- src/Components/MainCarousel/MainCarousel.tsx
+++ src/Components/MainCarousel/MainCarousel.tsx
@@ -16,52 +16,7 @@ const Preloader3 = ({res}:{res:any}) => {
     const router = useRouter()
     const [imgs,setImgs] = useState(
     
-            [
-
-                {
-                    img: `https://ucarecdn.com/173ac323-8645-43f4-8079-0f8a0612f764/mm1.jpg`,
-                    position:''
-                },
-        {
-            img: `https://images.pexels.com/photos/4031436/pexels-photo-4031436.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
-        },
-        // {
-        //     img: `https://ucarecdn.com/1deda759-6d5f-4999-b394-27bf3fbd85af/mm2.jpg`
-        // },
-        {
-            img: `https://ucarecdn.com/ecc189e8-0484-47a1-bc4c-55a2fc094f4c/mm8.jpg`
-        },
-     
-        {
-            img: `https://images.pexels.com/photos/8325971/pexels-photo-8325971.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
-        },
-        {
-            img: `https://images.pexels.com/photos/3762879/pexels-photo-3762879.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
-        },
-        {
-            img: `https://ucarecdn.com/7516192a-e6b7-4c6d-bee0-baa0f2540e02/mm9.jpg`
-        },
-        {
-            img: `https://images.pexels.com/photos/7020085/pexels-photo-7020085.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load`
-        },
-    {
-        img: `https://ucarecdn.com/5546c7dd-2df1-4786-903f-2d24a616372a/mm4.jpg`
-    },
-    {
-        img: `https://ucarecdn.com/bb0cf00f-f354-4dbf-beda-13f68197db9c/mm3.jpg`
-    },
-    {
-        img: `https://ucarecdn.com/5141fd15-a0b6-49ce-9383-8a99270c0d85/mm6.jpg`
-    },
-    {
-        img: `https://ucarecdn.com/7c227b6c-5aa7-43fd-a3df-e38fefb6a4f9/mm7.jpg`
-    },
-
-    //   {
-    //     img: 'https://images.pexels.com/photos/269583/pexels-photo-269583.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    //     position:'',
-    // }
-     ]
+         []
     )
      const redir = () => {
         router.push('/collection/products')
@@ -109,7 +64,7 @@ const Preloader3 = ({res}:{res:any}) => {
                 modules={[Autoplay]}
                 className="mySwiper swiper">
     
-                {imgs.map((item) => {
+                {imgs && imgs?.length > 0 && imgs.map((item : {img:string,position?:string}) => {
     
                     return <SwiperSlide 
                      
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,17 @@ import { Getproducts } from './Utils/Getproducts';
 const Home = async () => {
   // const categoriesData = await Getcategories()
   const productsResponse = await Getproducts()
-  // const ImagesResponse = await Getimages()
-  // DO NOT ask me why the data is nested the way it is
-  // I wrote it at 1am in the morning, HALF ASLEEP  
-  // console.log('categoriesData: ', categoriesData);
+  const ImagesResponse = await Getimages()
   
   // const categories = categoriesData?.success !== false && categoriesData?.Categories[0]?.cateArray;
-  // const images = ImagesResponse?.success !== false && ImagesResponse?.data?.Images[0]?.imagesArray;
+  const images = ImagesResponse?.success !== false && ImagesResponse?.data?.Images[0]?.imagesArray;
 
   return (
     <PreLoader
     categories={null}
     // categories={categories}
-    resImages={null}
-    // resImages={images}
+    // resImages={null}
+    resImages={images}
     // data={null}
     data={productsResponse?.data?.featuredProducts}
   />
